7c51203734b0503b27a5efc6ce43bcde77b70dee
dowon <djkim011337@gmail.com>
2025-10-20 13:12:32 +0900

    set cognito user pool

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_Kpcz1RIRm ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '6bhtnvo6bmku0f37gacec24srl' // e.g. us-east-2
+        userPoolClientId: '6bhtnvo6bmku0f37gacec24srl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'ap-northeast-2' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
