5c64b3f485aafd47b8b002a4f5bccdeac388705f
dowon <djkim011337@gmail.com>
2025-10-20 13:28:20 +0900

    set cognito user pool 10

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_ObmA5Or6a', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4elah5e88gsaru0vbv1g5gbfkh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '374t18128tagqbiaoehnjs20fr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
